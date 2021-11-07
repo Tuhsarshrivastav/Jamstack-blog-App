635be8ecc59d167eedd17dd543213601ed63452c
Tusharshrivastav <ts3657@gmail.com>
2021-11-07 09:54:14 +0530

    added files and scss we need

---
+++ components/PostWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostWidget = () => {
+    return (
+        <div>
+            postwdiget
+        </div>
+    )
+}
+
+export default PostWidget
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import 'tailwindcss/tailwind.css'
-
+import '../styles/globals.scss'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
